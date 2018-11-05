b0a92bba4cb14b285b46549623ac8cdba6882537
Ruben Young <rubeny484@gmail.com>
2018-11-05 08:37:11 -0500

    Restored my progress for the most part

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,43 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameplayState
+{
+    Pause,
+    NothingSelected,
+    PlayerSelected
+}
+
+/// <summary>
+/// Class to handle input from the player
+/// </summary>
 public class InputManager : MonoBehaviour {
 
-    BoardManager boardManager;
-    public bool TileSelection;
+    //BoardManager boardManager;
+    //public bool TileSelection;
 
 	// Use this for initialization
 	void Start () {
-        boardManager = GetComponent<BoardManager>();
-        TileSelection = false;
+        //boardManager = GetComponent<BoardManager>();
+        //TileSelection = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(TileSelection == true)
-        {
-            ChooseTile();
-        }
-        else
-        {
-            PlayerMovementPhase();
-        }
+        //if(TileSelection == true)
+        //{
+        //    ChooseTile();
+        //}
+        //else
+        //{
+        //    PlayerMovementPhase();
+        //}
 	}
 
-    private void PlayerMovementPhase()
-    {
-        boardManager.CheckPossibleMovement();
-    }
+    //private void PlayerMovementPhase()
+    //{
+    //    boardManager.CheckPossibleMovement();
+    //}
 
-    private void ChooseTile()
-    {
-        boardManager.MovePlayer();
-    }
+    //private void ChooseTile()
+    //{
+    //    boardManager.MovePlayer();
+    //}
 
-    private void CheckMouseIndex()
-    {
-        //Get the mouse's position in world space
-        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+    //private void CheckMouseIndex()
+    //{
+    //    //Get the mouse's position in world space
+    //    Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
 
-    }
+    //}
 }
